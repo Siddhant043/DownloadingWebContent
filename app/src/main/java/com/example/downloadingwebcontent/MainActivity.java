b0c618764ea9310f1d3d2a9713a984259587c9a6
Siddhant Mishra <siddhant48311@gmail.com>
2021-02-09 19:59:26 +0530

    Not Working

--- app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -6,14 +6,40 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    public class DownloadTask extends AsyncTask<String, Void, String> { // class which will do the download work
+ public class MainActivity extends AppCompatActivity {
+
+     public class DownloadTask extends AsyncTask<String, Void, String> { // class which will do the download work
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("URL", strings[0]); // printing the passed string with log
-            return "Done"; // this is the result which we put in result variable 
+        protected String doInBackground(String... urls) {
+            // Log.i("URL", strings[0]); // printing the passed string with log
+            // extracting html from URL
+            String result = null;
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]); // converting string into url
+                urlConnection = (HttpURLConnection) url.openConnection(); // connecting with the url
+                InputStream in = urlConnection.getInputStream(); //  creating a data stream with url
+                InputStreamReader reader = new InputStreamReader(in); // reading object
+                int data = reader.read();
+                while(data != -1){ // till document is ended
+                    char current = (char) data; // extracts character
+                    result += current; // adds char by char
+                    data = reader.read();
+                }
+                return result;
+            } catch(Exception e){
+                e.printStackTrace();
+                return "Failed";
+            }
         }
     }
 
@@ -25,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         DownloadTask task = new DownloadTask();
         String result = null;
         try {
-            result = task.execute("https://www.zappycode.com").get(); // passing the website string to the above function
+            result = task.execute("https://www.zappy.com").get(); // passing the website string to the above function
         } catch(Exception e){
             e.printStackTrace();
         }
